f4ee6ab97568b429175e6ccb53dc8ade32a9cbd9
engkoskostaman97 <engkoskostaman759@gmail.com>
2023-05-23 16:28:41 +0700

    create environment variabel docker\

--- web.go
+++ web.go
@@ -2,16 +2,21 @@ package main
 
 import "fmt"
 import "net/http"
+import "os"
+import "strings"
 
-func welcome (w http.ResponseWriter, r *http.Request){
-	fmt.Fprintln(w, "welcome to ENGKOS")
+func getallenv( w http.ResponseWriter,  r *http.Request){
+	for _, element:= range os.Environ(){
+		variabel := strings.Split(element,"=")
+		fmt.Fprintln(w, variabel[0],"=>",variabel[1])
+	}
 }
 
 func main(){
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintln(w, "Selamat Datang Bro")
+		fmt.Fprintln(w, os.Getenv("DB_PASSWORD"))
 	})
 
-	http.HandleFunc("/welcome", welcome)
+	http.HandleFunc("/getallenv",getallenv)
 	http.ListenAndServe(":8080",nil)
 }
